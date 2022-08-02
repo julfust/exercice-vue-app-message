e7bed786e18f4e679fa145f2ba17bb610b1ce7fa
Julien <jfustier42@gmail.com>
2022-08-02 10:20:53 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="global-container">
+    <Message initMessage="Hello world" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Message from "@/components/Message.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Message
   }
 }
 </script>
@@ -19,8 +19,13 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.global-container {
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,48 @@
+<script>
+
+export default ({
+    name: 'Message',
+    components: {},
+    props: {
+        initMessage: {
+            type: String
+        }
+    },
+    data() {
+        return {
+            message: ""
+        }
+    },
+    mounted() {
+        
+        if(!this.message) {
+            this.message = this.initMessage;
+        }
+    }
+})
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-content">{{ this.message }}</div>
+    </div>
+    <textarea 
+        name="" 
+        id="" 
+        cols="100" 
+        rows="10"
+        >
+    </textarea>
+</template>
+
+<style>
+  .card {
+    width: 32rem;
+    border-radius: 0.5rem;
+    background-color: #F8F8F8;
+    box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+    padding: 1rem;
+    margin-bottom: 2rem;
+    border: 1px solid black;
+  }
+</style>
